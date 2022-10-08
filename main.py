d19e0d732cc445ee9e7c485af9e05a8d835639c8
TheJPMZ <monzon.jpmz@gmail.com>
2022-10-07 21:10:17 -0600

    ✅Triangle done

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from numpy import diff, mat
 from gl import Raytracer
-from figures import REFLECTIVE, TRANSPARENT, Sphere, Intersect, Material, AABB
+from figures import OPAQUE, REFLECTIVE, TRANSPARENT, Sphere, Intersect, Material, AABB, Triangle
 from lights import AmbientLight, DirectionalLight, PointLight
 from object_literally import Texture
 
-width =  256
-height =  256
+width =  64
+height =  64
 
 def draw_cube(coords, size, material):
     x,y,z = coords
@@ -18,39 +18,24 @@ def draw_cube(coords, size, material):
     rtx.scene.append( AABB((x,y,z-tr), (size,size,0), material) )
     rtx.scene.append( AABB((x,y,z+tr), (size,size,0), material) )
 
-wall = Material(diffuse = "blue", spec = 32, matType=REFLECTIVE)
-roof = Material(diffuse = (0.6, 0.6, 0.6))
-back = Material(diffuse = (0.3, 0.3, 0.3))
-
-cube1 = Material(diffuse = "red")
-cube2 = Material(diffuse = "red", spec = 32, ior = 2, matType=TRANSPARENT)
-cube3 = Material(diffuse = "orange")
-cube4 = Material(diffuse = "orange", spec = 32, ior = 2, matType=TRANSPARENT)
+m1 = Material(diffuse = 'orange', spec = 64, matType = REFLECTIVE)
+m2 = Material(diffuse = 'green', spec = 64, matType = TRANSPARENT)
+m3 = Material(diffuse = 'magenta', spec = 64, matType = OPAQUE)
 
 rtx = Raytracer(width, height)
+rtx.envMap = Texture("mansion.bmp")
 
-#rtx.envMap = Texture("mansion.bmp")
-
-rtx.lights.append( AmbientLight(intensity = 0.5 ))
+rtx.lights.append( AmbientLight(intensity = 0.6 ))
 rtx.lights.append( PointLight(point=(0,0,-2)))
 
-z = 10
-size = 6
-tr = size/2
+rtx.scene.append( Triangle((-1.5,1,-7), (1.5,1,-7), (0,3,-7), m1) )
+rtx.scene.append( Triangle((1.5,1,-7), (2,2,-7), (0,3,-7), m1) )
 
-rtx.scene.append( AABB((tr,0,-z), (0.1,size,z), wall) )
-rtx.scene.append( AABB((-tr,0,-z), (0.1,size,z), wall) )
-rtx.scene.append( AABB((0,tr,-z), (size,0.1,z), roof) )
-rtx.scene.append( AABB((0,-tr,-z), (size,0.1,z), roof) )
+rtx.scene.append( Triangle((-1.5,-1,-7), (1.5,-1,-7), (0,1,-7), m2) )
+rtx.scene.append( Triangle((1.5,-1,-7), (2,0,-7), (0,1,-7), m2) )
 
-rtx.scene.append( AABB((0,0,-z), (size,size,0.1), back) )
-
-
-
-draw_cube((-1,-1,-5), 0.5, cube1)
-draw_cube((1,1,-5), 0.6, cube2)
-draw_cube((1,-1,-5), 0.7, cube3)
-draw_cube((-1,1,-5), 0.8, cube4)
+rtx.scene.append( Triangle((-1.5,-3,-7), (1.5,-3,-7), (0,-1,-7), m3) )
+rtx.scene.append( Triangle((1.5,-3,-7), (2,-2,-7), (0,-1,-7), m3) )
 
 rtx.glRender()
 
